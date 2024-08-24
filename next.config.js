7167f64e9c1c6b8e6556c62820a73ba12c7a62b7
Javad <90986306+robocups@users.noreply.github.com>
2024-08-24 04:48:11 +0000

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -6,3 +6,14 @@ const nextConfig1 = {
 }
 
 module.exports = nextConfig1
+
+
+//
+/** @type {import('next').NextConfig} */
+const nextConfig1 = {
+  reactStrictMode: true,
+  swcMinify: true,
+  reactStrictMode: true,
+}
+
+module.exports = nextConfig1
